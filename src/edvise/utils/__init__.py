b97edda9eb8b462e428d0c3ccafeb06df189b7b0
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-09-06 18:40:27 -0700

    Update __init__.py

--- src/edvise/utils/__init__.py
+++ src/edvise/utils/__init__.py
@@ -2,8 +2,7 @@ from . import (
     data_cleaning,
     databricks,
     drop_columns_safely,
-    infer_first_term_of_year,
-    infer_num_terms_in_year,
+    infer_data_terms,
     update_run_metadata_in_toml,
     types,
 )
