9d0b9391aaf6b8d0a04b85f886086497b47b7b4d
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-09-07 22:33:49 -0700

    fix reporting utils

--- src/edvise/reporting/model_card/h2o_pdp.py
+++ src/edvise/reporting/model_card/h2o_pdp.py
@@ -6,7 +6,7 @@ from edvise.modeling import h2o_ml
 from edvise.configs.pdp import PDPProjectConfig
 from edvise.reporting.model_card.base import ModelCard
 from edvise.reporting.sections.custom import register_sections as register_pdp_sections
-import edvise.reporting.utils as utils
+import edvise.reporting.utils as reporting_utils
 
 
 class H2OPDPModelCard(ModelCard[PDPProjectConfig]):
@@ -142,7 +142,7 @@ class H2OPDPModelCard(ModelCard[PDPProjectConfig]):
             ),
         }
         return {
-            key: utils.download_artifact(
+            key: reporting_utils.utils.download_artifact(
                 run_id=self.run_id,
                 description=description,
                 artifact_path=path,
