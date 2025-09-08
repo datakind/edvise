f8698eed1babba93d1fd2b433ad97405768cb415
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-09-07 19:40:10 -0700

    Update test_inference.py

--- tests/modeling/automl/test_inference.py
+++ tests/modeling/automl/test_inference.py
@@ -495,7 +495,7 @@ def test_empty_input():
         top_shap_features(features, unique_ids, shap_values)
 
 
-@patch("edvise.modeling.inference.select_top_features_for_display")
+@patch("edvise.modeling.automl.inference.select_top_features_for_display")
 @pytest.mark.parametrize(
     [
         "features",
