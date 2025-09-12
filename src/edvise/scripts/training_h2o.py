f154ac3d1d065081ce9b02b26421cdfd0a84e46e
kaylawilding <95330483+kaylawilding@users.noreply.github.com>
2025-09-12 08:26:08 -0700

    force include type check error

--- src/edvise/scripts/training_h2o.py
+++ src/edvise/scripts/training_h2o.py
@@ -113,9 +113,9 @@ class TrainingTask:
 
         modeling_cfg = self.cfg.modeling
         fs = modeling_cfg.feature_selection
-        force_include_vars = modeling_cfg.feature_selection.force_include_cols
-        # confirm each of the force_include_vars are in df_preprocessed columns
-        if force_include_vars is not None:
+        if modeling_cfg.feature_selection.force_include_cols is not None:
+            force_include_vars = modeling_cfg.feature_selection.force_include_cols
+            # confirm each of the force_include_vars are in df_preprocessed columns
             for var in force_include_vars:
                 if var not in df_preprocessed.columns:
                     raise ValueError(
