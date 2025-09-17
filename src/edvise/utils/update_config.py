28b6a82cc794d6fa924b3828f19bb4b8828c776a
Noreen Mayat <nm3224@alum.barnard.edu>
2025-09-17 10:38:05 -0700

    added key paths and new values

--- src/edvise/utils/update_config.py
+++ src/edvise/utils/update_config.py
@@ -77,7 +77,7 @@ class TomlConfigEditor:
 
     def update_key_course_subject_areas(self, cips: list[str]) -> None:
         self._merge_list_field(
-            ["preprocessing", "features", "key_course_subject_areas"], cips
+            key_path=["preprocessing", "features", "key_course_subject_areas"], new_values=cips
         )
 
 
