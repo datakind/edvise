4f780dfbff1f906eb0959ad17ac9d99a4645e760
Vishakh Pillai <vishpillai97@gmail.com>
2025-12-12 10:22:43 -0500

    fix: removing model type logic from pipeline

--- src/edvise/scripts/pdp_inf_prep.py
+++ src/edvise/scripts/pdp_inf_prep.py
@@ -149,16 +149,6 @@ class InferencePrepTask:
             verbose=True,
         )
 
-        # Setting h2o vs sklearn based on config
-        model_type = (
-            "h2o" if getattr(self.cfg.model, "framework", None) == "h2o" else "sklearn"
-        )
-        if dbutils:
-            dbutils.jobs.taskValues.set(key="model_type", value=model_type)
-            logger.info(f"Setting task parameter for 'model_type' as '{model_type}'")
-        else:
-            logger.info(f"(no dbutils) model_type resolved as '{model_type}'")
-
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
