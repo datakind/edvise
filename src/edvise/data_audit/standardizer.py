314cb42a99852b957b8b9257c4ca21c5a2ab07be
Noreen Mayat <nm3224@alum.barnard.edu>
2026-01-13 16:39:46 -0500

    feat: added assign_numeric_grade to pipeline

--- src/edvise/data_audit/standardizer.py
+++ src/edvise/data_audit/standardizer.py
@@ -183,6 +183,7 @@ class CustomCohortStandardizer(BaseStandardizer):
                 LOGGER.info("No duplicates found after keep_earlier_record.")
             else:
                 LOGGER.warning("Duplicates still remain after keep_earlier_record func. Investigate further.")
+        df = assign_numeric_grade(df)
         cols_to_drop = [
             # not all demographics used for target variable bias checks
             "incarcerated_status",
