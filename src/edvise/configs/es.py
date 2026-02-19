f9f730c431489928b883beb29908be5ae3f735be
Noreen Mayat <nm3224@alum.barnard.edu>
2026-02-19 12:48:08 -0800

    fix: removing es files

--- src/edvise/configs/es.py
+++
@@ -1,6 +0,0 @@
-"""ES (Enrollment/School) project config. Placeholder using PDP structure until ES-specific config is implemented."""
-
-from edvise.configs.pdp import InferenceConfig, PDPProjectConfig
-
-# Alias for ES projects; customize when ES-specific config is needed
-ESProjectConfig = PDPProjectConfig
