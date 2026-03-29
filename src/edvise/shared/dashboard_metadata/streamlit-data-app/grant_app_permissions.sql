a1ab110c9b415e3c97750ad604d45d44a9fbbfcd
Mesh <meshach.ogunmodede@datakind.org>
2026-03-29 15:04:58 -0500

    feat: added DABs for metadata dashboard

--- src/edvise/shared/dashboard_metadata/streamlit-data-app/grant_app_permissions.sql
+++
@@ -1,7 +0,0 @@
-GRANT USE CATALOG ON CATALOG `{{DB_workspace}}` TO `{{app_service_principal}}`;
-
-GRANT USE SCHEMA ON SCHEMA `{{DB_workspace}}`.`default` TO `{{app_service_principal}}`;
-
-GRANT SELECT ON TABLE `{{DB_workspace}}`.`default`.`pipeline_runs` TO `{{app_service_principal}}`;
-
-GRANT SELECT ON TABLE `{{DB_workspace}}`.`default`.`pipeline_models` TO `{{app_service_principal}}`;
