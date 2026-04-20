30d8fdc0e3c9b47072056e16f56169118c113f38
Vishakh Pillai <vishpillai97@gmail.com>
2026-04-20 19:30:36 -0400

    feat: adding hitl resolver for sma

--- src/edvise/genai/mapping/schema_mapping_agent/hitl/__init__.py
+++ src/edvise/genai/mapping/schema_mapping_agent/hitl/__init__.py
@@ -12,7 +12,11 @@ from edvise.genai.mapping.schema_mapping_agent.hitl.artifacts import (
     write_sma_manifest_artifact,
     write_sma_hitl_and_manifest_artifacts,
 )
-from edvise.genai.mapping.schema_mapping_agent.hitl.resolver import check_sma_hitl_gate
+from edvise.genai.mapping.schema_mapping_agent.hitl.resolver import (
+    SMAHITLResolverError,
+    check_sma_hitl_gate,
+    resolve_sma_items,
+)
 from edvise.genai.mapping.schema_mapping_agent.hitl.schemas import (
     HITL_CONFIDENCE_THRESHOLD,
     InstitutionSMAHITLItems,
@@ -37,7 +41,9 @@ __all__ = [
     "SMA_MANIFEST_OUTPUT_BASENAME",
     "SMARRunEvent",
     "add_alias_if_missing",
+    "SMAHITLResolverError",
     "check_sma_hitl_gate",
+    "resolve_sma_items",
     "load_sma_hitl",
     "load_sma_manifest_output",
     "unique_sma_hitl_items_by_item_id",
