5b06a1a654d3594ed48d94381f2c3b3aec78f841
kaylawilding <kayla.wilding@datakind.org>
2026-04-22 17:23:11 -0700

    add datetime handling

--- src/edvise/scripts/es_data_audit.py
+++ src/edvise/scripts/es_data_audit.py
@@ -7,7 +7,6 @@ import sys
 import pandas as pd
 import pathlib
 import os
-from functools import partial
 
 # Go up 3 levels from the current file's directory to reach repo root
 script_dir = os.getcwd()
@@ -23,22 +22,20 @@ print("Repo root:", repo_root)
 print("src_path:", src_path)
 print("sys.path:", sys.path)
 
-from edvise.data_audit.schemas import RawEdviseCohortDataSchema, RawEdviseCourseDataSchema
+from edvise.data_audit.schemas import RawPDPCohortDataSchema, RawPDPCourseDataSchema
 from edvise.data_audit.standardizer import (
     ESCohortStandardizer,
     ESCourseStandardizer,
 )
 from edvise.utils.databricks import get_spark_session
-from edvise.utils.data_cleaning import handling_duplicates
-
 from edvise.dataio.path_management import pick_existing_path
 from edvise.dataio.read import (
     read_config,
-    read_raw_es_cohort_data,
-    read_raw_es_course_data,
+    read_raw_pdp_cohort_data,
+    read_raw_pdp_course_data,
 )
 from edvise.dataio.write import write_parquet
-from edvise.configs.pdp import ESProjectConfig
+from edvise.configs.es import ESProjectConfig
 from edvise.data_audit.eda import (
     compute_gateway_course_ids_and_cips,
     log_record_drops,
@@ -73,6 +70,8 @@ class ESDataAuditTask:
     def __init__(
         self,
         args: argparse.Namespace,
+        course_converter_func: t.Optional[ConverterFunc] = None,
+        cohort_converter_func: t.Optional[ConverterFunc] = None,
     ):
         self.args = args
         self.cfg = read_config(
@@ -81,6 +80,8 @@ class ESDataAuditTask:
         self.spark = get_spark_session()
         self.cohort_std = ESCohortStandardizer()
         self.course_std = ESCourseStandardizer()
+        self.course_converter_func: t.Optional[ConverterFunc] = course_converter_func
+        self.cohort_converter_func: t.Optional[ConverterFunc] = cohort_converter_func
 
     def run(self):
         """Executes the data preprocessing pipeline."""
@@ -116,15 +117,24 @@ class ESDataAuditTask:
         # --- Load RAW datasets ---
         LOGGER.info(" Loading raw cohort and course datasets:")
 
-        # Raw cohort data
-        df_cohort_raw = read_raw_pdp_cohort_data(
-            file_path=cohort_dataset_raw_path,
-            schema=None,
-            spark_session=self.spark,
-        )
-
-        # Raw course data
         dttm_formats = ["ISO8601", "%Y%m%d.0"]
+
+        for fmt in dttm_formats:
+            try:
+                df_cohort_raw = read_raw_pdp_cohort_data(
+                    file_path=cohort_dataset_raw_path,
+                    schema=None,
+                    dttm_format=fmt,
+                    spark_session=self.spark,
+                )
+                break
+            except ValueError:
+                continue
+        else:
+            raise ValueError(
+                " Failed to parse cohort data with all known datetime formats."
+            )
+
         for fmt in dttm_formats:
             try:
                 df_course_raw = read_raw_pdp_course_data(
@@ -146,8 +156,7 @@ class ESDataAuditTask:
             ("Raw cohort", df_cohort_raw),
             ("Raw course", df_course_raw),
         ]:
-            require(len(df_cohort_raw) > 0, f"{label} dataset is empty (0 rows).")
-            require(len(df_course_raw) > 0, f"{label} dataset is empty (0 rows).")
+            require(len(df) > 0, f"{label} dataset is empty (0 rows).")
 
         LOGGER.info(
             " Loaded raw cohort and course data: checking for mismatches in cohort and course files: "
@@ -170,12 +179,22 @@ class ESDataAuditTask:
 
         # Schema validate cohort data
         LOGGER.info(" Reading and schema validating cohort data:")
-        df_cohort_validated = read_raw_pdp_cohort_data(
-            file_path=cohort_dataset_raw_path,
-            schema=RawPDPCohortDataSchema,
-            converter_func=self.cohort_converter_func,
-            spark_session=self.spark,
-        )
+        for fmt in dttm_formats:
+            try:
+                df_cohort_validated = read_raw_pdp_cohort_data(
+                    file_path=cohort_dataset_raw_path,
+                    schema=RawPDPCohortDataSchema,
+                    dttm_format=fmt,
+                    converter_func=self.cohort_converter_func,
+                    spark_session=self.spark,
+                )
+                break
+            except ValueError:
+                continue
+        else:
+            raise ValueError(
+                " Failed to parse cohort data with all known datetime formats (validated pass)."
+            )
 
         # Standardize cohort data
         LOGGER.info(" Standardizing cohort data:")
@@ -414,7 +433,7 @@ if __name__ == "__main__":
         LOGGER.info("Running task default course converter func")
         LOGGER.warning(f"Failed to load custom converter functions: {e}")
 
-    task = PDPDataAuditTask(
+    task = ESDataAuditTask(
         args,
         cohort_converter_func=cohort_converter_func,
         course_converter_func=course_converter_func,
